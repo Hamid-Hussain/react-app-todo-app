eca50c2650fa3a45df0d2e9d00685371f7c1d138
HamidHussain02 <61314876+HamidHussain02@users.noreply.github.com>
2023-02-02 08:24:48 +0500

    initial commit~

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Todo from './components/Todo'
+
+function App() {
+  return (
+    <div className="">
+      <h1>My Todos</h1>
+      <Todo text='Learn React'/>
+      
+      
+    </div>
+  ); 
+}
+
+export default App;
